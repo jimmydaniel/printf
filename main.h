5a9579a49fa898a637ffca75804ed25b05e95ba3
Haimanot Eshete <haimanoteshete685@gmail.com>
2022-11-22 14:11:00 -0800

    group project

--- main.h
+++ main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 /**
  * struct print - structure for printing various types
  * @t: type to print
@@ -29,5 +34,6 @@ int print_S(va_list S);
 int print_r(va_list r);
 int print_R(va_list R);
 int print_r(va_list r);
+int get_flags(const char *format, int *i);
 
 #endif
